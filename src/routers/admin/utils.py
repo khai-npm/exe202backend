d502b2b0e742bed10588a5e068ff00de8273a80e
yeuMyrrh <manhkhai2606@gmail.com>
2024-11-09 17:15:35 +0700

    transaction premium hotfix

--- src/routers/admin/utils.py
+++ src/routers/admin/utils.py
@@ -28,13 +28,10 @@ async def action_get_all_account():
 async def action_get_admin_analysis():
     total_user = await account.find_all().to_list()
     total_user_num = len(total_user)
-    active_user_num : int = 0
-    for i in total_user:
-        detect_active = await server.find_one(server.server_owner == i.discord_user_id)
-        if detect_active:
-            active_user_num = active_user_num + 1
-    premium_token = await token.find_all().to_list()
-    premium_user_num = len(premium_token)
+    active_status = await account.find_many(account.status == True).to_list()
+    active_user_num = len(active_status)
+    premium_user = await account.find_many(account.payment_info_id != []).to_list()
+    premium_user_num = len(premium_user)
 
     percentage_premium_user = (premium_user_num/total_user_num)*100
     percentage_user_rate = (active_user_num/total_user_num)*100
